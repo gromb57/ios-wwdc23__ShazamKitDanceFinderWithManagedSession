303f9367bef25e815be1dcd571dfd6433666f214
gromb57 <jeromeb57@gmail.com>
2023-06-15 21:31:04 +0200

    e

---
+++ Shared Source/Extensions/ShazamKitDanceFinderLibrary.swift
@@ -0,0 +1,13 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+Extension for ShazamKit library items.
+*/
+
+import ShazamKit
+
+extension SHLibrary {
+    
+    var items: [SHMediaItem] { [] }
+}
